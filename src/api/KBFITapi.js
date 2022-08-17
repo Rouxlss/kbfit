bfdf594c7bf8d9fa0cc6e6e2a3f99cd31981d242
Arnoldo Ortíz <adolfo.flores@focusservices.com>
2022-08-17 12:54:15 -0600

    feat: sending authorization headers

--- src/api/KBFITapi.js
+++ src/api/KBFITapi.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import Cookies from "js-cookie";
 
 export const KBFITapi = axios.create({
-    baseURL: `http://localhost:4000/api/v1`
+    // baseURL: `http://localhost:4000/api/v1`
     // baseURL: `https://kfi-backend.vercel.app/api/v1`,
-    // baseURL: `https://kfi-api.ga/api/v1`,
+    baseURL: `https://kfi-api.ga/api/v1`,
     // withCredentials: true
 });
